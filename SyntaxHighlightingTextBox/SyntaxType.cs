2aef3bc0be5216cc9edb65510f327dbdb26aacca
leasange <leasange@qq.com>
2015-11-08 15:13:43 +0800

    add synhtb

---
+++ SyntaxHighlightingTextBox/SyntaxType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ls.SyntaxHighlighting
+{
+    public enum SyntaxType
+    {
+        SQL,//sql
+        CSharp,//C#
+    }
+}
